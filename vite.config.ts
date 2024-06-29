67fc528911155b2feda51792e658853afaacad00
Lucas Costa <englucascbmelo@gmail.com>
2024-06-29 18:59:43 -0300

    primeira modificação e testando se a configuração do deploy deu certo

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/psicologa_nayanne_melo"
+  base: "/psicologa_nayanne_melo",
 })
