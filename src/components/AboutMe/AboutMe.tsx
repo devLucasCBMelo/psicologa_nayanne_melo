736dc470b6b3c72c690142dc8ed6be764d2d1989
Lucas Costa <englucascbmelo@gmail.com>
2024-07-05 18:55:43 -0300

    criando a primeira branch e subindo com várias modificações

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,9 +1,11 @@
+import styles from "./aboutme.module.css";
+
 function AboutMe() {
   return (
-    <>
+    <div className={ styles.about_me}>
       <h2>Sobre mim</h2>
       <p>Psicóloga formada na UFPB. Atuei na Clínica yyyyy onde pude atender vários pacientes e etc.</p>
-    </>
+    </div>
   )
 }
 
