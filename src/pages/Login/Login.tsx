047be45eb9b2c75e9addab2e6e86c852b19a5b24
Lucas Costa <englucascbmelo@gmail.com>
2024-07-06 20:50:48 -0300

    inserção do segundo container

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -5,6 +5,7 @@ import fotoPerfil from "../../images/foto_de_perfil.jpg";
 // import fotoLivro from "../../images/foto_com_livro.jpg";
 import styles from "./Login.module.css"
 import whatsappLogo from "../../images/whatsapp_logo.png";
+import nayphone from "../../images/nayphone-removebg.png";
 
 function Login() {
   return (
@@ -24,11 +25,36 @@ function Login() {
 
           <button className={ styles.consulta }>
             <img className={ styles.consulta_img } src={ whatsappLogo } alt="logo do whatsapp" />
-            Agende a sua primeira consulta</button>
+            <h4>Agende a sua primeira consulta</h4>
+          </button>
         </div>
 
-        <div>
-          <h2> Entenda as vantagens da terapia online</h2>
+        <div className={ styles.container_2 }>
+          <div className={ styles.container_2_left}>
+            <img src={ nayphone } alt="nayanne no telefone" />
+          </div>
+          <div className={ styles.container_2_div }>
+            <h2 className={ styles.lora_xablau}>Entenda as vantagens da terapia online</h2>
+            <div className={ styles.container_2_div_number }>
+              <h2>1</h2>
+              <h3>No conforto da sua casa</h3>
+            </div>
+
+            <div className={ styles.container_2_div_number }>
+              <h2>2</h2>
+              <h3>Oferece maior flexibilidade</h3>
+            </div>
+
+            <div className={ styles.container_2_div_number }>
+              <h2>3</h2>
+              <h3>Garante o apoio emocional</h3>
+            </div>
+
+            <div className={ styles.container_2_div_number }>
+              <h2>4</h2>
+              <h3>Minimizar a distância</h3>
+            </div>
+          </div>
         </div>
       
         <AboutMe />
