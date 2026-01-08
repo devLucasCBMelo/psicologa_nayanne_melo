f6c1fc2bb7c89c6210f04beafa76d2257de87929
Lucas Costa <englucascbmelo@gmail.com>
2026-01-08 12:02:50 -0300

    nova estilização do main hero

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,9 +5,11 @@ function Header() {
   return (
     <div className={`${styles.main_header}`}>
       <h1>Psicóloga Nayanne Melo</h1>
-      <h1>Início</h1>
-      <h1>Sobre</h1>
-      <h1>Agendar</h1>
+      <nav className={styles.navBar}>
+        <h1>Início</h1>
+        <h1>Sobre</h1>
+        <h1>Agendar</h1>
+      </nav>
       <LoginButton />
     </div>
   );
