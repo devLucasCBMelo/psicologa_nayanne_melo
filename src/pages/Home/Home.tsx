a91fd77e1501181b92004330e05202bdc273f2c7
Lucas Costa <englucascbmelo@gmail.com>
2026-01-14 17:17:17 -0300

    atualizando o footer

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,9 @@
-import AboutMe from "../../components/AboutMe/AboutMe";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import styles from "./Home.module.css";
-import nayphone from "../../images/nayphone-removebg.png";
 import { MainHero } from "../../components/MainHero/MainHero";
 import { SecondHero } from "../../components/SecondHero/SecondHero";
+import { ArrowRight, Calendar } from "lucide-react";
 
 function Home() {
   return (
@@ -13,37 +12,27 @@ function Home() {
       <div className={styles.mainHome}>
         <MainHero />
         <SecondHero />
-        <div className={styles.container_2}>
-          <div className={styles.container_2_left}>
-            <img src={nayphone} alt="nayanne no telefone" />
-          </div>
-          <div className={styles.container_2_div}>
-            <h2 className={styles.lora_xablau}>
-              Entenda as vantagens da terapia online
+
+        <section className={styles.container_4}>
+          <div className={styles.container_4_div}>
+            <h2>
+              Comece a sua jornada de <span>transformação hoje</span>
             </h2>
-            <div className={styles.container_2_div_number}>
-              <h2>1</h2>
-              <h3>No conforto da sua casa</h3>
-            </div>
-
-            <div className={styles.container_2_div_number}>
-              <h2>2</h2>
-              <h3>Oferece maior flexibilidade</h3>
-            </div>
-
-            <div className={styles.container_2_div_number}>
-              <h2>3</h2>
-              <h3>Garante o apoio emocional</h3>
-            </div>
-
-            <div className={styles.container_2_div_number}>
-              <h2>4</h2>
-              <h3>Minimizar a distância</h3>
-            </div>
+            <p>
+              O primeiro passo é sempre o mais importante. Agende sua consulta e
+              permita-se cuidar da sua saúde mental.
+            </p>
+            <button>
+              <Calendar className={styles.calendarIcon} />
+              <span>Agendar Consulta</span>
+              <ArrowRight className={styles.arrowRightIcon} />
+            </button>
+            <p className={styles.container_4_last_p}>
+              Primeira sessão com desconto especial
+            </p>
           </div>
-        </div>
+        </section>
 
-        <AboutMe />
         <Footer />
       </div>
     </>
